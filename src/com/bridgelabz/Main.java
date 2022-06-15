3607c3c5c161d51eedc07aea03d59cb0e4e17701
RohitChugwani <rchugwani3@gmail.com>
2022-06-15 11:29:54 +0530

    made object to test addressbookfunctions,added details using paramatrized function and displayed it

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to address book");
+        AddressBookFunctions mohit = new AddressBookFunctions();
+        mohit.createContact("mohit","raj","Samadhiya colony","gwalior","MP","123","mohit@gmail.com","474001");
+        mohit.show();
     }
 }
