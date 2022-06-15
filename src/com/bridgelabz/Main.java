7114dc5ce311223dce3078788f1dbbdc6fbe0df4
RohitChugwani <rchugwani3@gmail.com>
2022-06-15 12:15:28 +0530

    created array list to store the details

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,10 +1,10 @@
 package com.bridgelabz;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Welcome to address book");
-        AddressBookFunctions mohit = new AddressBookFunctions();
-        mohit.createContact("mohit","raj","Samadhiya colony","gwalior","MP","123","mohit@gmail.com","474001");
-        mohit.show();
+        ArrayList<AddressBookFunctions> database = new ArrayList<AddressBookFunctions>();
     }
 }
